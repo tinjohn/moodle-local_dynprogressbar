a91bb5707cabd19170a5014d61bf9c7587a2c3b2
tinjohn <tina.john@th-luebeck.de>
2023-06-26 14:23:10 +0200

    the lang file

---
+++ lang/en/local_dynprogressbar.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/**
+ * @package  nattylocal
+ * @copyright 2017, Mohammed Essaid MEZERREG <me@mohessaid.com>
+ * @license MIT
+ * @doc https://docs.moodle.org/dev/String_API
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Dynamic Progressbar';
